0f42001620fb7fdccf03270b888eaec4ae89e855
frengkyfangidae <43429759+frengkyfangidae@users.noreply.github.com>
2018-10-29 02:44:30 +0700

    Add files via upload

---
+++ pantai.php
@@ -0,0 +1,81 @@
+<?php
+echo "<body bgcolor='151414' >";
+echo "<h1 align='center' style='font-family:corbel;'><font color='CCFFFF' size='11'>Pantai Indah di Indonesia yang Wajib Dikunjungi</h1>";
+echo "<p align='center'><table width='600'>
+<tr>
+<td><h1 align='left' style='font-family:corbel;'><font color='silver'>Pink Beach-Pulau Lombok</font></h1></td>
+</tr>
+<tr>
+<td><p align='center'><img src='pink.jpg' width='600' height='290' /></p></td></tr>
+<tr>
+<td><p align='justify' style='font-family:corbel;'><font color='C0C0C0'>Sesuai dengan namanya, pantai ini memiliki keunikan pasirnya yang 
+berwarna merah muda. Itulah yang membuat pantai ini begitu istimewa di mata banyak pengunjung.
+Menjadi berwarna merah muda karena pasir putihnya sudah bercampur dengan pasir berwarna merah.
+Percaya atau tidak, pantai dengan pasir warna tersebut cuma ada tujuh yang bisa ditemukan di 
+dunia dan jelas yang paling cantik adalah Indonesia punya. Kesenangan pengunjung tidak hanya
+sebatas menikmati pasir yang berwarna merah muda, tapi pemandangan lautnya yang berwarna biru 
+agak kehijauan pun sangat bening. Kegiatan berenang di lautnya pun menjadi aman karena ombaknya 
+termasuk tenang dan kecil sehingga aman untuk anak-anak. Selain dari berenang, para wisatawan 
+pun bisa snorkeling sepuasnya karena di perairan Pink Beach memiliki beragam biota laut yang wajib 
+ditemui. Namun, segala alat untuk snorkeling harus dibawa sendiri oleh pengunjung. Tidak ketinggalan, ada sebuah meriam kuno dan 
+goa buatan yang bisa ditemukan di area Pantai Pink yang merupakan peninggalan penjajahan Jepang.
+Di pinggir pantai, ada sejumlah fasilitas umum yang bisa memenuhi kebutuhan para wisatawan,
+termasuk warung makan.
+</font></td>
+</tr>
+</table> <br/><br/>";
+
+echo "<p align='center'><table width='600'>
+<tr>
+<td><h1 align='left' style='font-family:corbel;'><font color='silver'>Tanjung Bira-Sulawesi Selatan</font></h1></td>
+</tr>
+<tr>
+<td><p align='center'><img src='tanjungbira.jpg' width='600' height='290' /></p></td></tr>
+<tr>
+<td><p align='justify' style='font-family:corbel;'><font color='C0C0C0'>Pantai yang keindahannya tidak perlu diragukan
+ lagi adalah Tanjung Bira di mana area pantai yang rapi dan bersih ini sangat mengaumkan 
+ dengan tekstur lembut hamparan pasirnya. Berlokasi di ujung selatan Pulau Sulawesi, siapapun 
+ yang datang ke tempat ini akan dibuat terpukau dan nyaman dengan adanya pohon kelapa berjajar 
+ berikut kokohnya bukit karang. Kegiatan seru yang bisa membuat lupa waktu di Tanjung Bira adalah
+ berjemur, snorkeling, menyelam, dan berenang.
+Menonton matahari terbit dan terbenam juga menjadi kegiatan yang diminati oleh banyak wisatawan.
+ Bonusnya, ada Pulau Kambing dan Pulau Liukang yang keindahannya pun bisa dinikmati oleh para 
+ wisatawan. Dengan lokasinya yang berada di Kabupaten Bulukumba, tepatnya di Kecamatan Bonto
+ Bahari, Tanjung Bira merupakan sebuah daerah pantai yang sudah lengkap segala akomodasi dan 
+ fasilitasnya. Di tempat ini, para pengunjung bisa menyewa motor, menggunakan kamar mandi yang
+ nyaman, serta menyewa peralatan menyelam. Pelabuhan kapal ferry pun tersedia untuk mengantarkan 
+ setiap pengunjung yang berkeinginan untuk melakukan kegiatan diving di Pulau Selayar.
+Bagi yang ingin menginap di dekat pantai juga terdapat banyak penginapan yang bisa dipilih, 
+mulai dari villa sampai hotel yang biayanya pun beragam. Jika merasa lapar juga tidak ada
+ masalah, di sekitar pantai terdapat rumah makan yang bisa memuaskan hasrat wisata kuliner 
+Anda.
+</font></td>
+</tr>
+</table> <br/><br/>";
+
+echo "<p align='center'><table width='600'>
+<tr>
+<td><h1 align='left' style='font-family:corbel;'><font color='silver'>Pantai Tanjung Tinggi-Pulau Belitung</font></h1></td>
+</tr>
+<tr>
+<td><p align='center'><img src='tanjungtg.jpg' width='600' height='290' /></p></td></tr>
+<tr>
+<td><p align='justify' style='font-family:corbel;'><font color='C0C0C0'>Untuk para pecinta film yang berjudul Laskar 
+Pelangi pasti tahu dengan sebuah pantai yang bernama Tanjung Tinggi. Terdapat begitu 
+banyak jejeran bebatuan granit dalam ukuran besar yang akan menangkap perhatian kita 
+di sekitar sepanjang bibir pantai. Bila ditanya daya tarik apakah yang membuat begitu 
+banyak pengunjung jatuh hati pada pantai ini, maka jawabannya tentu adalah set tersebut.
+ Pantai Tanjung Tinggi hanya perlu dtempuh sekitar 31 kilometer dari kota Tanjung Pandan 
+ dan lokasinya pun sangat mudah diakses.
+Hanya dalam waktu 40 menit, wisatawan akan sampai ke pantai ini dan segala keindahannya,
+ termasuk pasir putih akan menyambut kedatangan Anda bersama keluarga maupun teman-teman
+ saat mengunjungi Pulau Belitung. Pulau ini pun dikenal sebagai pulau yang menghasilkan
+ pertambangan mineral jenis C, seperti halnya tanah liat putih atau kaolin, pasir 
+ kuarsa dan timah putih selain dari bebatuan granit. Lada putih juga diketahui berasal
+ dari sini. Kejernihan warna lautnya yang biru dan pasirnya yang selembut salju adalah
+ yang tidak boleh terlewat ketika menginjakkan kaki di Pantai Tanjung Tinggi di Pulau
+ Belitung.
+</font></td>
+</tr>
+</table>";
+?>
